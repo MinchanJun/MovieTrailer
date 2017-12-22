a0af2ac08b1d2e1438b186ae434c3868cf1ea8a9
Minchan <minchanjun@gmail.com>
2017-12-22 20:52:04 +0900

    Documentation edited

--- media.py
+++ media.py
@@ -1,37 +1,74 @@
 class Video():
     '''
-    Parent Class Video that has 2 children Movie class and Tv class
-    Attributes: title, box_art, poster_image_url, trailer_youtube_url
+    Parent Class Object that stores Video related information.
     '''
     def __init__(self, title, box_art, poster_image_url, trailer_youtube_url):
+        '''
+        Initialize instance of class Video
+
+        :param title: string
+        :param box_art: string
+        :param poster_image_url: string
+        :param trailer_youtube_url: string
+        '''
         self.title = title
         self.box_art = box_art
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
+        """
+        Initializing instance for opening the youtube video
 
+        :return: webbrowser to play trailer
+        """
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 
 class Movie(Video):
     '''
-    Child class Movie that is inherited from Video class
-    Attributes: title, box_art, poster_image_url, trailer_youtube_url, and its own movie_hours
+    Child Class Object that is inherited from Parent Class Object
     '''
     def __init__(self,title,box_art,poster_image_url,trailer_youtube_url,movie_hours):
+        '''
+        Initialize instance of class Movie
+
+        :param title: string (from parent class)
+        :param box_art: string (from parent class)
+        :param poster_image_url: string (from parent class)
+        :param trailer_youtube_url: string (from parent class)
+        :param movie_hours: int
+        '''
         Video.__init__(self,title,box_art,poster_image_url,trailer_youtube_url)
         self.movie_hours = movie_hours
 
     def show_trailer(self):
+        '''
+        Initializing instance for opening the youtube video
+
+        :return: webbrowser to play trailer
+        '''
         webbrowser.open(self.trailer_youtube_url)
 
 class Tv(Video):
     '''
-    Child class Tv that is inherited from Video class
-    Attributes: title, box_art, poster_image_url, trailer_youtube_url and its own broadcaster
+    Child Class Object that is inherited from Parent Class Object
     '''
     def __init__(self,title,box_art,poster_image_url,trailer_youtube_url,broadcaster):
+        '''
+        Initialize instance of class Tv
+
+        :param title: string (from parent class)
+        :param box_art: string (from parent class)
+        :param poster_image_url: string (from parent class)
+        :param trailer_youtube_url: string (from parent class)
+        :param movie_hours: int
+        '''
         Video.__init__(self,title,box_art,poster_image_url,trailer_youtube_url)
         self.broadcaster = broadcaster
 
     def show_trailer(self):
+        '''
+        Initializing instance for opening the youtube video
+
+        :return: webbrowser to play trailer
+        '''
         webbrowser.open(self.trailer_youtube_url)
